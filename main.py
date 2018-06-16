aca53c28ba2d36915f4a89414a1eaad95bc33456
Joel Kaaberg <joel.kaberg@gmail.com>
2018-06-16 19:49:54 +0000

    line graph instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import dateutil.parser
-import datetime
+ # -*- coding: utf-8 -*-
 
-from pygal import Bar as pygal_bar
+from pygal import Line as pygal_line
 from csv import reader as csv_reader
 from os import environ
 from gevent.pywsgi import WSGIServer
@@ -11,49 +10,51 @@ app = Flask(__name__)
 
 def readCsv(f_path):
     speedstats = []  
-    with open(f_path) as csvfile:
+    with open(f_path, 'rt', encoding='utf-8') as csvfile:
         speedreader = csv_reader(csvfile)
         for row in speedreader:
-            server = row[1]
-            location = row[2]
-            timestamp = row[3]
-            distance = row[4]
-            ping = row[5]
-            downspeed = float(row[6])/1000000
-            upspeed = float(row[7])/1000000
-     
-            speedstats.append(dict(server=server, location=location, timestamp=timestamp, distance=distance, ping=ping, downspeed=downspeed, upspeed=upspeed))
+            speedstats.append(dict(server=row[1], 
+                                   location=row[2], 
+                                   timestamp=row[3], 
+                                   distance=row[4], 
+                                   ping=row[5], 
+                                   downspeed=float(row[6])/1000000, 
+                                   upspeed=float(row[7])/1000000))
         return speedstats
 
-def createChart(speedstats):
-    speedchart = pygal_bar(y_title='Mbps', truncate_legend=-1)
-    speedchart.x_labels = 'Up', 'Down'
+def createChart(stats):
+    chart = pygal_line(y_title='Mbps', x_title='Time', legend_at_bottom=True, dots_size=1)
     
-    # Add bars
-    average_down = float()
-    average_up = float()
-    for s in speedstats:
-      average_down = average_down + s['downspeed']
-      average_up = average_up + s['upspeed']
-      speedchart.add(s['server'] + ' (' + s['location'] + ')', [s['downspeed'], s['upspeed']])
-     
-    # Calculate and add average
-    average_down = average_down / len(speedstats)
-    average_up = average_up / len(speedstats)
-    speedchart.add('Average', [average_down, average_up])
-     
-    # Convert UTC to more readble local time and set title
-    utc = speedstats[0]['timestamp']
-    utc = dateutil.parser.parse(utc)
-    from_zone = dateutil.tz.tzutc()
-    to_zone = dateutil.tz.tzlocal()
-    utc = utc.replace(tzinfo=from_zone)
-    localtime = utc.astimezone(to_zone)
-    title = localtime.strftime("%Y-%m-%d %H:%M:%S %Z")
-    speedchart.config(title=title)
-     
-    # Render and save to png
-    return speedchart.render_data_uri()
+    time = []
+    speeds = {}
+    avg_down = float()
+    avg_up = float()
+
+    for s in stats:
+        avg_down += s['downspeed']
+        avg_up += s['upspeed']
+#        time.append(s['timestamp'])
+
+        if s['server'] in speeds.keys():
+            speeds[s['server']]['down'].append(s['downspeed'])
+            speeds[s['server']]['up'].append(s['upspeed'])                    
+        else:
+            speeds[s['server']] = {'up': [s['upspeed']], 'down': [s['downspeed']]}
+
+    # Add servers and plot speeds
+    for server, speed in speeds.items():
+        chart.add(server, speed['down'])
+        chart.add(server, speed['up'], secondary=True)
+    
+    # Add avarage speeds
+    avg_down = avg_down / len(stats)
+    avg_up = avg_up / len(stats)
+    chart.add('Avarage', [avg_down])
+    chart.add('Avarage', [avg_up], secondary=True)
+#    chart.x_labels = time
+
+    # Render
+    return chart.render_data_uri()
 
 @app.route('/')
 def index():
@@ -63,3 +64,5 @@ def index():
 if __name__ == '__main__':
     http = WSGIServer(('0.0.0.0', 8080), app.wsgi_app)
     http.serve_forever()
+
+    #createChart(readCsv(environ['CSV_PATH']))
